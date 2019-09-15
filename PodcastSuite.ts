7ac946d56d1bfba40381473ab4fd0a6e8dd358d1
Alberto 🙈 <alnav@amazon.com>
2019-09-15 17:18:56 -0400

    Updating Format

--- PodcastSuite.ts
+++ PodcastSuite.ts
@@ -1,6 +1,7 @@
 import xml2js from 'xml2js';
 import DB from "./DB";
 import StreamReader from './StreamReader';
+import Format from './Format';
 
 export interface IPodcastSuiteConfig {
     proxy?: IProxy;
@@ -25,7 +26,7 @@ export interface IPodcast {
     length?: number;
 }
 
-interface IRSS {
+export interface IRSS {
     rss: {
         [key: string]: any
     }
@@ -167,105 +168,8 @@ class PodcastSuite {
     @param json: IRSS obect to be converted into IPodcast Ojbect
     @return IPodcast Object
     */
-    public static format(json: IRSS, init:{ length?: number, url?:string } = { length: Date.now() } ): IPodcast{
-        const channel = Array.isArray(json.rss.channel) ?
-                        json.rss.channel[0] : 
-                        json.rss.channel;
-        const rss: any = Object.assign(init, { items: [], created: Date.now() }); 
-
-        if (channel.title) {
-            rss.title = channel.title[0];
-        }
-
-        if (channel.description) {
-            rss.description = channel.description[0];
-        }
-        if (channel.link) {
-            rss.link = channel.link[0];
-        }
-    
-        if (channel.image) {
-            rss.image = channel.image[0].url
-        }
-    
-        if (!rss.image && channel["itunes:image"]) {
-            rss.image = channel['itunes:image'][0].href
-        }
-        rss.image =  Array.isArray(rss.image) && rss.image[0] || rss.image;
-                
-        if (channel.item) {
-            if (!Array.isArray(channel.item)) {
-                channel.item = [channel.item];
-            }
-            channel.item.forEach(function (val) {
-                const obj: {[key:string]: any} = {};
-                obj.title = (val.title) ? val.title[0] : '';
-                obj.description = (val.description) ? val.description[0] : '';
-                obj.url = obj.link = (val.link) ? val.link[0] : '';
-                obj.guid = val.guid && val.guid[0] && (val.guid[0]['_'] || val.guid[0]);
-    
-                if (val['itunes:subtitle']) {
-                    obj.subtitle = val['itunes:subtitle'][0];
-                }
-                if (val['itunes:summary']) {
-                    obj.summary = val['itunes:summary'][0];
-                }
-                if (val['itunes:author']) {
-                    obj.author = val['itunes:author'][0];
-                }
-                if (val['itunes:explicit']) {
-                    obj.explicit = val['itunes:explicit'][0];
-                }
-                if (val['itunes:duration']) {
-                    obj.duration = val['itunes:duration'][0];
-                }
-                if (val['itunes:season']) {
-                    obj.season = val['itunes:season'][0];
-                }
-                if (val['itunes:episode']) {
-                    obj.episode = val['itunes:episode'][0];
-                }
-                if (val['itunes:episodeType']) {
-                    obj.episodeType = val['itunes:episodeType'][0];
-                }
-                if(val['itunes:image']) {
-                    const hasImageHref = (val['itunes:image'][0] &&
-                                          val['itunes:image'][0].$ &&
-                                          val['itunes:image'][0].$.href);
-                    const image = hasImageHref ? val['itunes:image'][0].$.href : null;
-                    obj.image = image; 
-                }
-                if (val.pubDate) {
-                    obj.created = Date.parse(val.pubDate[0]);
-                }
-                if (val['media:content']) {
-                    obj.media = val.media || {};
-                    obj.media.content = val['media:content'];
-                }
-                if (val['media:thumbnail']) {
-                    obj.media = val.media || {};
-                    obj.media.thumbnail = val['media:thumbnail'];
-                }
-    
-                if (val.enclosure) {
-                    obj.enclosures = [];
-                    if (!Array.isArray(val.enclosure))
-                        val.enclosure = [val.enclosure];
-                        val.enclosure.forEach(function (enclosure) {
-                        var enc = {};
-                        for (const x in enclosure) {
-                            enc[x] = enclosure[x][0];
-                        }
-                        obj.enclosures.push(enc);
-                    });
-                  obj.media = obj.enclosures.length > 0 ? obj.enclosures[0] : null;
-                }
-                rss.items.push(obj);
-            });
-        }
-        
-        return rss
-    }
+   
+    public static format = Format;
 
     /*
     Get Podcast Data from DB, if it doesn't exist it will fetch it.
